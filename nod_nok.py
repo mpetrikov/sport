c0128c00b04d20024563a51e4e026eb4d9ff3d6a
Max Petrikov <petrikovmn@gmail.com>
2019-02-02 02:19:52 +0300

    Наибольший общий делитель, наименьшее общее кратное

---
+++ nod_nok.py
@@ -0,0 +1,18 @@
+a = int(input())
+b = int(input())
+
+a1 = a
+b1 = b
+while a1 != 0 and b1 != 0:
+    if a1 > b1:
+        a1 %= b1
+    else:
+        b1 %= a1
+# наибольший общий делитель
+nod = a1 + b1
+# наименьшее общее кратное
+nok = a * b / nod
+print(int(a * b / nod))
+
+
+
